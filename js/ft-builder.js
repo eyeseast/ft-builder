d5a0601873988f94baf4eca66905781c2b591c53
Chris Amico <eyeseast@gmail.com>
2011-07-21 13:28:22 -0400

    Initial form prototype

---
+++ js/ft-builder.js
@@ -0,0 +1,15 @@
+/***
+Fusion Table Builder
+--------------------
+
+This is a port of Google's [Fusion Table Layer Builder](http://gmaps-samples.googlecode.com/svn/trunk/fusiontables/fusiontableslayer_builder.html), using jQuery, Backbone and Underscore, with the aim of integrating it into the WordPress admin.
+***/
+
+/***
+Models
+***/
+
+
+/***
+Views
+***/
